75a016203b57db04d705e84a0e28bdca982a3722
Oliver <rift95@live.se>
2019-07-15 23:00:02 +0200

    added support for command-only-channels

---
+++ src/commands/command-only-channels.ts
@@ -0,0 +1,30 @@
+import { Client, Message } from 'discord.js';
+import { isCommand } from '../util/commandUtils';
+
+// TODO: Make this part dynamic via a command such as !coc [channel name]
+const eventSignChannelID = '597864917441183754';
+const channels = [ eventSignChannelID ].reduce((res, v) => ({...res, [v]: v}), {});
+
+export const commands = [];
+export const ID = 'command-only-channels';
+export const callback = (client: Client) => {
+  const messageHandler = async (message: Message) => {
+    if (isCommand(message.content) || message.author.bot) {
+      // commands are always OK
+      return;
+    }
+
+    if (message.channel.id in channels) {
+      // None command messages are not allowed in blacklisted channels
+
+      console.debug(`Removed a message from a command-only-channels:`, message.content);
+      await message.author.send(
+        `A message you wrote have been removed due to restrictions put on the channel.\n` +
+        `Only messages containing commands for ME (the sometimes-helpful BOT) may be posted in the given channel.`,
+      );
+
+      await message.delete(); // Clean up command
+    }
+  };
+  client.on('message', messageHandler);
+};
