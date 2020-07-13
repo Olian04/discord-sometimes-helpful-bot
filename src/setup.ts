baa308e8ae413a58e9fb5e66ba52397c33720ff7
Oliver <rift95@live.se>
2020-07-13 18:20:03 +0200

    added env logging

--- src/setup.ts
+++ src/setup.ts
@@ -1,9 +1,16 @@
 import dotenv from 'dotenv';
-dotenv.config();
 import betterLogging from 'better-logging';
+
+// Populating environment variables
+const err = dotenv.config().error;
+
+// Setting up logging library
 betterLogging(console, {
   format: (ctx) => `${ctx.date} ${ctx.time24} ${ctx.type} ${ctx.msg}`,
 });
+
+if (err) console.error(`An error occurred when configuring environment variables: ${err}`);
+else console.info(`Environment variables configured`);
 
 // Enable debug logging if in development mode
 console.logLevel = process.env.DEPLOY_ENVIRONMENT === 'development' ? 4 : 3;
