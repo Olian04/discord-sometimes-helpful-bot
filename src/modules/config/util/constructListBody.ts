838504dce9e3529ff9d991a3e7468c5fe6728054
Oliver <rift95@live.se>
2020-09-10 14:27:17 +0200

    removed remnents of old solution

--- src/modules/config/util/constructListBody.ts
+++ src/modules/config/util/constructListBody.ts
@@ -1,9 +1,6 @@
 import { Whitelist } from '../interfaces/WhiteList';
 
 export const constructListBody = (title: string, whiteList: Whitelist): string => {
-  if (whiteList.length === 0) {
-    return '```text'
-  }
   return title + '\n' +
   '```fix\n' +
     whiteList.map(v => `- ${v}`).join('\n')
